dd4e9401d4b4bb963cc0501edc7d5af13979934b
Melusi Khumalo <melusi.l.k@gmail.com>
2018-05-17 15:10:27 +0200

    Initial Commit

---
+++ src/App.vue
@@ -0,0 +1,67 @@
+<template>
+      <div>
+        <intro></intro>
+        <div id="body">
+          <div id="current">
+            <current v-bind:currentCurrency="currentCurrency"></current>
+          </div>
+          <div id="previous">
+            <previous v-bind:previousCurrency="previousCurrency"></previous>
+          </div>
+        </div>
+      </div>
+    </template>
+    <script>
+        import Intro from './components/Intro.vue';
+        import Current from './components/Current.vue';
+        import Previous from './components/Previous.vue';
+        export default {
+          name: 'app',
+          components: {Intro, Current, Previous},
+          data() {
+            return {
+              currentCurrency: {BTC: '', ETH: '', LTC: ''},
+              previousCurrency: {
+                yesterday: {}, twoDays: {}, threeDays: {}, fourDays: {}, fiveDays: {}
+              }
+            }
+          },
+          methods: {
+            // Stub
+          },
+          created() {
+            // Stub
+          }
+        }
+        </script>
+        <style>
+    @import url('https://fonts.googleapis.com/css?family=Lato');
+    * {
+      margin : 0px;
+      padding : 0px;
+      font-family: 'Lato', sans-serif;
+    }
+    body { height: 100vh; width: 100%; }
+    .row { display: flex; flex-wrap: wrap; }
+    h1 { font-size: 48px; }
+    a { color: #FFFFFF; text-decoration: none; }
+    a:hover { color: #FFFFFF; }
+    a:visited { color: #000000; }
+    .button {
+      margin: auto;
+      width: 200px;
+      height: 60px;
+      border: 2px solid #E36F55;
+      box-sizing: border-box;
+      border-radius: 30px;
+    }
+    #body {
+      max-width: 90%;
+      margin: 0 auto;
+      padding: 1.5em;
+      text-align: center;
+      color:rgb(0, 193, 131);
+    }
+    #current { padding: 2em 0em; }
+    #previous { padding: 2em 0em; }
+    </style>
---
+++ src/components/Previous.vue
@@ -0,0 +1,60 @@
+<template>
+      <div>
+        <h2>Previous prices of coins</h2>
+        <div id="first">
+          <h2>Date:   {{previousCurrency.yesterday.DATE}}</h2>
+          <p><label>1 BTC:</label> {{previousCurrency.yesterday.BTC}}</p>
+          <p><label>1 ETH:</label> {{previousCurrency.yesterday.ETH}}</p>
+          <p><label>1 LTC:</label> {{previousCurrency.yesterday.LTC}}</p>
+        </div>
+        <div id="second">
+          <h2>Date:   {{previousCurrency.twoDays.DATE}}</h2>
+          <p><label>1 BTC:</label> {{previousCurrency.twoDays.BTC}}</p>
+          <p><label>1 ETH:</label> {{previousCurrency.twoDays.ETH}}</p>
+          <p><label>1 LTC:</label> {{previousCurrency.twoDays.LTC}}</p>
+        </div>
+        <div id="third">
+          <h2>Date:   {{previousCurrency.threeDays.DATE}}</h2>
+          <p><label>1 BTC:</label> {{previousCurrency.threeDays.BTC}}</p>
+          <p><label>1 ETH:</label> {{previousCurrency.threeDays.ETH}}</p>
+          <p><label>1 LTC:</label> {{previousCurrency.threeDays.LTC}}</p>
+        </div>
+        <div id="fourth">
+          <h2>Date:   {{previousCurrency.fourDays.DATE}}</h2>
+          <p><label>1 BTC:</label> {{previousCurrency.fourDays.BTC}}</p>
+          <p><label>1 ETH:</label> {{previousCurrency.fourDays.ETH}}</p>
+          <p><label>1 LTC:</label> {{previousCurrency.fourDays.LTC}}</p>
+        </div>
+        <div id="fifth">
+          <h2>Date:   {{previousCurrency.fiveDays.DATE}}</h2>
+          <p><label>1 BTC:</label> {{previousCurrency.fiveDays.BTC}}</p>
+          <p><label>1 ETH:</label> {{previousCurrency.fiveDays.ETH}}</p>
+          <p><label>1 LTC:</label> {{previousCurrency.fiveDays.LTC}}</p>
+        </div>
+      </div>
+    </template>
+    <script>
+        export default {
+          name: 'app',
+          props: {
+            previousCurrency: { type: Object }
+          },
+          data () {
+            return {}
+          }
+        }
+        </script>
+        <style scoped>
+    #first, #second, #third, #fourth, #fifth {
+      border: 1px solid #F5CE00;
+      padding: 2em 0em;
+      max-width: 90%;
+      margin: 3px auto;
+    }
+    #first p, #second p, #third p, #fourth p, #fifth p {
+      display: inline-block;
+      padding: 0em 1.5em;
+      font-size: 1.5rem;
+    }
+    h2 { font-size: 1.5em; }
+    </style>
